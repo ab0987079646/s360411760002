57bf57bead871aae89ac9ff922a6a35d069b2818
unknown <ab0987079646@gmail.com>
2018-06-26 15:07:31 +0700

    Test upload

---
+++ src/lab1/mycode.java
@@ -0,0 +1,18 @@
+package lab1;
+
+public class mycode {
+    //main
+    public static void main(String[] args) {
+
+    System.out.println("Hell world");
+    System.out.println("thaweesak ratsadorn");
+    System.out.println("Mtt421 ");
+    System.out.println("s360411760002");
+    System.out.println("RMUTSV");
+
+
+
+    }//main
+
+
+}//class
