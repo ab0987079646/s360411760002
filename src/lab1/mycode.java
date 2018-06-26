e298c01abee7e2f7b6cc792a0554cc49d93a183d
Labcom4 <ab0987079646@gmail.com>
2018-06-26 15:40:15 +0700

    test email

--- src/lab1/mycode.java
+++ src/lab1/mycode.java
@@ -16,3 +16,7 @@ public class mycode {
 
 
 }//class
+
+
+
+
---
+++ src/lab1/mycode2.java
@@ -0,0 +1,12 @@
+package lab1;
+
+public class mycode2 {//main
+
+    //main
+    public static void main(String[] args) {
+        System.out.println("ab0987079646@gmail.com");
+
+
+    }//main
+}
+
