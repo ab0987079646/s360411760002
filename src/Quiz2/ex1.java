b7ccd27b03f89ad49c196daabc038cdf39bf1190
Rock_Roll <ab0987079646@gmail.com>
2018-10-16 20:13:09 +0700

    Quiz 2 3

--- src/Quiz2/ex1.java
+++ src/Quiz2/ex1.java
@@ -1,7 +1,9 @@
 package Quiz2;
 
-import java.util.Scanner;
 
+
+
+import java.util.Scanner;
 
 //ทรงกลม
 // ทรงกระบอก
@@ -9,10 +11,66 @@ import java.util.Scanner;
 // ทรงพีระมิด
 // ทรงปริซึม
 public class ex1 {
-    public static double  ทรงกลม (double x,double y) {
-        return 4/3*3.14* x*y; }
-    Scanner input = new Scanner(System.in);
-    System.
+
+    private static    final   double PT = 3.1414 ;
+    private static Scanner sc = new Scanner(System.in);
+
+    private static void Sphere (){
+        double r,v;
+
+        System.out.println("ป้อนค่ารัศมี (r) เป็นเซนติเมตร: ");
+        r = sc.nextDouble();
+        v = 4/3*PT* (r*r*r);
+        System.out.println("ปริมาตวงกลม : " +v);
+
+    }//Sphere
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+        int select = 0;
+
+
+
+
+         do {
+             System.out.println("กรุณาเลือกรูปแบบที่คุณต้องการ");
+             System.out.println("1.ทรงกลม");
+             System.out.println("2.ทรงกระบอก");
+             System.out.println("3.ทรงกรวย");
+             System.out.println("4.ทรงพีระมิด");
+             System.out.println("5.ทรงปริซึม");
+             System.out.println("select:");
+
+             select = sc.nextInt();
+
+
+             switch (select) {
+                 case 1:
+                     Sphere();
+                     break;
+                 default:
+                     System.out.println("กดใหม่"); break;
+
+             }//switch
+
+         } while ((select >= 1 ) && (select <= 6 ));
+
+
+
+
+
+
+
+
+        }//main
+
+
+
 
 
 
---
+++ src/Quiz3/Quiz3.java
@@ -0,0 +1,35 @@
+package Quiz3;
+
+import java.util.Scanner;
+
+public class Quiz3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //input
+        //Fahrenheit
+        System.out.print(" Fahrenheit?: ");
+        double Fahrenheit = scanner.nextDouble();
+        //Celsius
+        double Celsius = (5.0/9.0) * (Fahrenheit -32.0);
+
+        //output
+        System.out.println("Celsius is"+Celsius);
+
+        System.out.println("Celsius is"+Celsius);
+        System.out.println("Celsius is"+Celsius);
+        System.out.println("Celsius is"+Celsius);
+
+
+
+
+
+
+
+    }//main
+
+
+
+
+}//class
+
+
